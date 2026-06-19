d16ce75371f7402a84900d56f3579cd2054423f7
Vinicius Rodrigues Cordeiro <vrc_crv@outlook.com.br>
2026-06-18 23:08:30 -0300

    feat: adiciona to_dict e from_dict na classe Tarefa

--- model/tarefa.py
+++ model/tarefa.py
@@ -15,18 +15,18 @@ class Status(Enum):
     CANCELADA = "Cancelada"
 
 class Tarefa:
-    def __init__(self, titulo, descricao, prioridade, prazo):
-        self.__id = uuid.uuid4()
+    def __init__(self, titulo, descricao, prioridade, prazo, status=None, id_tarefa=None, criado_em=None):
+        self.__id_tarefa = id_tarefa if id_tarefa is not None else uuid.uuid4()
         self.__titulo = titulo
         self.__descricao = descricao
         self.__prioridade = prioridade
-        self.__status = Status.PENDENTE
+        self.__status = status if status is not None else Status.PENDENTE
         self.__prazo = prazo
-        self.__criado_em = datetime.datetime.now()
+        self.__criado_em = criado_em if criado_em is not None else datetime.datetime.now()
 
     @property
     def id(self):
-        return self.__id
+        return self.__id_tarefa
 
     @property
     def titulo(self):
@@ -71,3 +71,28 @@ class Tarefa:
     @property
     def tarefa_concluida(self):
         return self.__status == Status.CONCLUIDA
+
+    def to_dict(self):
+        return {
+            "id_tarefa": self.__id_tarefa,
+            "titulo": self.__titulo,
+            "descricao": self.__descricao,
+            "prioridade": self.__prioridade.value,
+            "status": self.__status.value,
+            "prazo": self.__prazo.isoformat(),
+            "criado_em": self.__criado_em.isoformat(),
+
+        }
+
+
+    @classmethod
+    def from_dict(cls, dicionario):
+        return cls(
+            id_tarefa=dicionario["id_tarefa"],
+            titulo=dicionario["titulo"],
+            descricao=dicionario["descricao"],
+            prioridade=Prioridade(dicionario["prioridade"]),
+            status=Status(dicionario["status"]),
+            prazo=datetime.datetime.fromisoformat(dicionario["prazo"]),
+            criado_em=datetime.datetime.fromisoformat(dicionario["criado_em"]),
+        )
